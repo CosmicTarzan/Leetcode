e13a43f690f0641362946d6adf0503c154e27bc4
CosmicTarzan <adameko@gmail.com>
2023-12-23 00:01:59 +0100

    Create 0118. Pascal's Triangle.py

---
+++ 0118. Pascal's Triangle.py
@@ -0,0 +1,16 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        if numRows == 1:
+            return [[1]]
+        elif numRows == 2:
+            return [[1],[1, 1]]
+        else:
+            ans = [[1],[1, 1]]
+            for i in range(2, numRows):
+                line = [1]
+                for j in range(1, i):
+                    line.append(ans[i - 1][j - 1] + ans[i - 1][j]) 
+                line.append(1)
+                ans.append(line)
+
+        return ans
